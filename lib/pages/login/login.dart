46bea028e72e0c6def727e5f5504f4f420dbfd65
aeser9o1 <aashiskumar986@gmail.com>
2019-06-22 00:06:35 +0530

    login card

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -3,23 +3,39 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: new BoxDecoration(
-          image: new DecorationImage(
-              image: new AssetImage("login.jpg"), fit: BoxFit.fill),
-        ),
-        child: new Column(
-          children: <Widget>[
-            new Center(
+    return Stack(children: <Widget>[
+      Container(
+          decoration: new BoxDecoration(
+        image: new DecorationImage(
+            image: new AssetImage("login.jpg"), fit: BoxFit.fill),
+      )),
+      Center(
+        child: Container(
+            height: 200,
+            child: Card(
                 child: Container(
-              child: Card(
-                color: Colors.white,
-                child: Column(
-                  children: <Widget>[Text("Login")],
-                ),
+              padding: EdgeInsets.all(18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Login",
+                    style: new TextStyle(
+                        fontFamily: 'Nunito',
+                        fontSize: 21,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 7),
+                    child: Text(
+                      "Have an existing bag code?",
+                      style: new TextStyle(fontFamily: 'Nunito'),
+                    ),
+                  )
+                ],
               ),
-            ))
-          ],
-        ));
+            ))),
+      ),
+    ]);
   }
 }
