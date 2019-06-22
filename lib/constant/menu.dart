17258bd35a905612896cec9837c8ad7ce7ec06ae
Tushar Sadana <tushar.169107181@muj.manipal.edu>
2019-06-22 16:55:08 +0200

    menu added

--- lib/constant/menu.dart
+++ lib/constant/menu.dart
@@ -1,9 +1,9 @@
 class Menu {
   var menu = [
-    {"name": "", "price": "", "barcode": ""},
-    {"name": "", "price": "", "barcode": ""},
-    {"name": "", "price": "", "barcode": ""},
-    {"name": "", "price": "", "barcode": ""},
-    {"name": "", "price": "", "barcode": ""}
+    {"name": "Jeu de Cartes", "price": "1.2", "barcode": "3700299931371"},
+    {"name": "Notebook", "price": "3.0", "barcode": "3254569163172"},
+    {"name": "Evian Bottle", "price": "1.5", "barcode": "3068320099651"},
+    {"name": "Trippex", "price": "1.92", "barcode": "3086126100685"},
+    {"name": "Halls", "price": "1.00", "barcode": "7622210609779"}
   ];
 }
